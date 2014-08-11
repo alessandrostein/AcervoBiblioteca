a31a8d3e728e612a84e9e96864c0e8d17c95c993
alessandrostein <alessandro@alessandrostein.com>
2014-08-11 08:51:06 -0300

    Alterado livros

--- src/org/catolicasc/acervo/dao/LivroDAO.java
+++ src/org/catolicasc/acervo/dao/LivroDAO.java
@@ -12,21 +12,31 @@ public class LivroDAO {
 		List<Livro> livros = new ArrayList<Livro>();
 		
 		Livro livro1 = new Livro();
-		livro1.setNome("Rails");
-		livro1.setResumo("Aprendo Rails na prática");
-		livro1.setEditora("Editora NovaTec");
+		livro1.setNome("A Startup Enxuta");
+		
+		livro1.setResumo("Eric Ries criou uma abordagem revolucionária de administração "
+					   + "que esta transformando a maneira pela qual os novos produtos  "
+					   + "são criados, desenvolvidos e lançados.                        ");
+		
+		livro1.setEditora("Lua de Papel");
 		livro1.setAnoPublicacao(2012);
-		livro1.setAutores(Arrays.asList("Paul Deitel", "Harvey Liro"));
+		livro1.setAutores(Arrays.asList("Eric Ries"));
 		livros.add(livro1);
 		
 		Livro livro2 = new Livro();
-		livro2.setNome("Rails");
-		livro2.setResumo("Aprendo Rails na prática");
-		livro2.setEditora("Editora NovaTec");
-		livro2.setAnoPublicacao(2012);
-		livro2.setAutores(Arrays.asList("Paul Deitel", "Harvey Liro"));
+		livro2.setNome("Coletânea Front-end: Uma antologia da comunidade front-end brasileira");
+		
+		livro2.setResumo("Um grupo de 11 autores de renome nacional na comunidade se juntou "
+				       + "para escrever artigos que julgamos relevantes para a Web.         ");
+		
+		livro2.setEditora("Casa do Código");
+		livro2.setAnoPublicacao(2014);
+		livro2.setAutores(Arrays.asList("Almir Filho", "Bernard De Luna", "Caio Gondim", 
+										"Deivid Marques", "Eduardo Shiota", "Giovanni Keppelen", 
+										"Luiz Corte Real", "Jaydson Gomes", "Reinaldo Ferraz", 
+										"Sergio Lopes"));
 		livros.add(livro2);
-				
+		
 		return livros;		
 	}
 }
